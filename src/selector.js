8cc9aea852b7eb096abe17b1183bdb39ccae7e55
AnastaSAV <anastasia.personalbox@gmail.com>
2021-12-23 17:54:04 +0200

    added function

--- src/selector.js
+++ src/selector.js
@@ -1,5 +1,9 @@
-const selector = (obj, keys) => {
-    // Ваше решение должно быть здесь
+const selector = function (obj, keys) {
+	for (let i = 0; i < keys.length; i++) {
+		if (!obj[keys[i]]) return '';
+		else obj = obj[keys[i]];
+	}
+	return obj;
 };
 
 module.exports = selector;
